a4a3272ddacacdae40d811bdbffc6b53f67cd70f
Mateus Macedo <mateusmacedoeng@gmail.com>
2025-06-28 13:11:42 -0300

    add button component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from "./components/navbar/navbar";
+import { PrimaryButton } from "./components/primary-button/primary-button";
+import { SecondaryButton } from "./components/secondary-button/secondary-button";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar],
+  imports: [RouterOutlet, Navbar, PrimaryButton, SecondaryButton],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
