18ae0cf0b858e9917732791466fe4424447b31f5
Mateus Macedo <mateusmacedoeng@gmail.com>
2025-07-04 18:12:21 -0300

    retrieving form data with ngModel and formsModule

--- src/app/pages/certificado-form/certificado-form.ts
+++ src/app/pages/certificado-form/certificado-form.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { PrimaryButton } from "../../components/primary-button/primary-button";
 import { SecondaryButton } from "../../components/secondary-button/secondary-button";
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-certificado-form',
-  imports: [PrimaryButton, SecondaryButton],
+  imports: [PrimaryButton, SecondaryButton, FormsModule],
   templateUrl: './certificado-form.html',
   styleUrl: './certificado-form.css'
 })
 export class CertificadoForm {
-
+  nome: string = '';
+  atividade: string = '';
+  atividades: string[] = ['ola', 'bom dia', 'boa tarde', 'boa noite'];
 }
