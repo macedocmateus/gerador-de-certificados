47ffea657d398325f6633ce82467080874533a32
Mateus Macedo <mateusmacedoeng@gmail.com>
2025-07-04 16:52:37 -0300

    add define routes

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,17 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from "./components/navbar/navbar";
-import { PrimaryButton } from "./components/primary-button/primary-button";
-import { SecondaryButton } from "./components/secondary-button/secondary-button";
-import { ItemCertificado } from "./components/item-certificado/item-certificado";
+
+
+
 import { BaseUi } from "./components/base-ui/base-ui";
-import { Certificados } from "./pages/certificados/certificados";
-import { CertificadoForm } from "./pages/certificado-form/certificado-form";
-import { Certificado } from "./pages/certificado/certificado";
+
+
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar, PrimaryButton, SecondaryButton, ItemCertificado, BaseUi, Certificados, CertificadoForm, Certificado],
+  imports: [RouterOutlet, Navbar, BaseUi],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
