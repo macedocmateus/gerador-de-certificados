9a326ec40111a684412395147b49a19be0849bba
Mateus Macedo <mateusmacedoeng@gmail.com>
2025-07-05 18:00:51 -0300

    add validating form fields

--- src/app/pages/certificado-form/certificado-form.ts
+++ src/app/pages/certificado-form/certificado-form.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { PrimaryButton } from "../../components/primary-button/primary-button";
 import { SecondaryButton } from "../../components/secondary-button/secondary-button";
-import { FormsModule } from '@angular/forms';
+import { FormsModule, NgModel } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-certificado-form',
-  imports: [PrimaryButton, SecondaryButton, FormsModule],
+  imports: [PrimaryButton, SecondaryButton, FormsModule, CommonModule],
   templateUrl: './certificado-form.html',
   styleUrl: './certificado-form.css'
 })
@@ -13,4 +14,8 @@ export class CertificadoForm {
   nome: string = '';
   atividade: string = '';
   atividades: string[] = ['ola', 'bom dia', 'boa tarde', 'boa noite'];
+
+  campoInvalido(control: NgModel) {
+    return control.invalid && control.touched
+  }
 }
